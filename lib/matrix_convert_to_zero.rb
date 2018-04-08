04572d131c8dd799b450244d11e2809e8c2600f4
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-04-08 12:20:22 -0700

    Update variable names

--- lib/matrix_convert_to_zero.rb
+++ lib/matrix_convert_to_zero.rb
@@ -2,7 +2,6 @@
 # Assumption/ Given: All numbers in the matrix are 0s or 1s
 # If any number is found to be 0, the method updates all the numbers in the
 # corresponding row as well as the corresponding column to be 0.
-require 'pry'
 def matrix_convert_to_0(matrix)
   rows_with_zero = []
   columns_with_zero = []
@@ -23,34 +22,34 @@ def matrix_convert_to_0(matrix)
     row += 1
   end
 
-  index_one = 0
-  while index_one < rows_with_zero.length
-    row = rows_with_zero[index_one]
-    inner = 0
-    while inner < matrix[row].length
-      matrix[row][inner] = 0
-      inner += 1
+  row_index = 0
+  while row_index < rows_with_zero.length
+    row = rows_with_zero[row_index]
+    column = 0
+    while column < matrix[row].length
+      matrix[row][column] = 0
+      column += 1
     end
-    index_one += 1
+    row_index += 1
   end
 
-  index = 0
-  while index < matrix.length
+  row_index = 0
+  while row_index < matrix.length
 
     column = 0
-    while column < matrix[index].length
+    while column < matrix[row_index].length
 
-      inner = 0
-      while inner < columns_with_zero.length
-        if column == columns_with_zero[inner]
-          matrix[index][column] = 0
+      column_index = 0
+      while column_index < columns_with_zero.length
+        if column == columns_with_zero[column_index]
+          matrix[row_index][column] = 0
         end
-        inner += 1
+        column_index += 1
       end
 
       column += 1
     end
-    index += 1
+    row_index += 1
   end
 
   return matrix
