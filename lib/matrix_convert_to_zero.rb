75666d5074243ebb14bb25cbd6ec5c9d7cbed082
Lily Sky <lilyxsky@gmail.com>
2018-04-22 12:49:32 -0700

    tests pass

--- lib/matrix_convert_to_zero.rb
+++ lib/matrix_convert_to_zero.rb
@@ -3,5 +3,33 @@
 # If any number is found to be 0, the method updates all the numbers in the
 # corresponding row as well as the corresponding column to be 0.
 def matrix_convert_to_0(matrix)
-  raise NotImplementedError
+  return false if matrix.nil?
+
+  rows = matrix.length
+  columns = matrix[0].length
+
+  select_columns = []
+
+  rows.times do |i|
+    columns.times do |j|
+      if matrix[i][j] == 0
+        k = 0
+        until k == columns
+          matrix[i][k] = 0
+          k += 1
+        end
+        select_columns << j
+      end
+    end
+  end
+
+  select_columns.each do |column|
+    q = 0
+    until q == rows
+      matrix[q][column] = 0
+      q += 1
+    end
+  end
+
+  return matrix
 end
